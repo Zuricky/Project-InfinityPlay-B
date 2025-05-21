87b0a531b0ee5a62f6a1e60e5ee86363c911cd34
Luca Zuin <zuin.luca1111@gmail.com>
2025-05-21 12:28:58 +0200

    feat(auth): finalize custom security package

---
+++ src/main/java/com/project_infinityplay/backend/auth/configs/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.project_infinityplay.backend.auth.configs;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Consente tutte le richieste su tutti gli endpoint
+                        .allowedOrigins("*") // Consente richieste da qualsiasi origine
+                        .allowedMethods("*") // Consente tutti i metodi (GET, POST, PUT, DELETE, PATCH, OPTIONS)
+                        .allowedHeaders("*") // Consente tutti gli header
+                        .allowCredentials(false); // Consente l'invio di credenziali, mettere false se allowedOrigins è "*"
+            }
+        };
+    }
+}
---
+++ src/main/java/com/project_infinityplay/backend/auth/configs/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.project_infinityplay.backend.auth.configs;
+
+import com.project_infinityplay.backend.auth.app_user.AppUser;
+import com.project_infinityplay.backend.auth.app_user.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Utente non trovato con username: " + username));
+
+        return appUser;
+
+    }
+}
